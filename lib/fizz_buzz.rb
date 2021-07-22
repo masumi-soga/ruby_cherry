b23083b937192c598b77669a64fe60a779f1c8a9
masumi-soga <sg.kse777@gmail.com>
2021-07-22 10:17:41 +0900

    fizz_buzz_test追加

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -2,26 +2,11 @@ def fizz_buzz(num)
   if num % 15 == 0
     "Fizz Buzz"
   elsif num % 3 == 0
-    "Buzz"
-  elsif num % 5 == 0
     "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
   else
     num.to_s
   end
 end
 
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(7)
-puts fizz_buzz(15)
-
-puts "------"
-a = "Hello"
-b = "Hello"
-a = b
-a.upcase!
-puts a,b
