a45455575dfbc7d0a0d5c8ad73ed06e947841af9
masumi-soga <sg.kse777@gmail.com>
2021-07-28 16:00:04 +0900

    to_intsメソッド実装

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -17,3 +17,18 @@ def to_hex(r, g, b)
     hex + n.to_s(16).rjust(2, "0")
   end
 end
+
+def to_ints(hex)
+  r = hex[1..2]
+  g = hex[3..4]
+  b = hex[5..6]
+  # リファクタリング前
+  # ints = []
+  # [r,g,b].each do |s|
+  #   ints << s.hex
+  # end
+  # ints
+  # リファクタリング後
+  # mapメソッドを使用
+  [r,g,b].map{|s| s.hex}
+end
