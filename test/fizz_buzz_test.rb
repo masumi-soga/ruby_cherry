053dc8ee1687ed715ad433f7d01838524509e1ab
masumi-soga <sg.kse777@gmail.com>
2021-08-01 12:38:13 +0900

    テスト追加

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -9,6 +9,7 @@ class FizzBuzzTest < Minitest::Test
     assert_equal '4', fizz_buzz(4)
     assert_equal 'Buzz', fizz_buzz(5)
     assert_equal 'Fizz', fizz_buzz(6)
+    assert_equal '7', fizz_buzz(7)
     assert_equal 'Fizz Buzz', fizz_buzz(15)
   end
 end
